67404dee2de9981b1d18432ef0829aba4337068f
Faeze <>
2024-11-20 15:37:09 +0330

    [add] types

---
+++ src/app/types.ts
@@ -0,0 +1,14 @@
+// src/types.ts or app/types.ts
+export type Product = {
+    id: string;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+      rate: number;
+      count: number;
+    };
+  };
+  
