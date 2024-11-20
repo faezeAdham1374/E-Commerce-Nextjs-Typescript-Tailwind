e4782c9cc54bbbc0c381b097c4e46f008c5a2f05
Faeze <>
2024-11-20 15:36:48 +0330

    [fix] get data from api in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Home from '@/Containers/Home';
+import { fetchProducts } from '@/hooks/useProducts';
+import { Product } from '@/app/types';
 
-import Home from '@/Containers/Home'
-export default function Page() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+export default async function Page() {
+  try {
+    const data: Product[] = await fetchProducts();
+    return (
+      <div>
+        <Home data={data} isLoading={false} error={null} />
+      </div>
+    );
+  } catch (error: any) {
+    return (
+      <div>
+        <Home data={undefined} isLoading={false} error={error} />
+      </div>
+    );
+  }
 }
